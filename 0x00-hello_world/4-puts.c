b6a14c455a551a2c3a21751ab0c7cd7f506e4a0f
DinoTech01 <destinyeromosele2018@gmail.com>
2022-06-30 16:00:50 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 
@@ -10,7 +9,7 @@ int main(void)
 
 {
 
-	 puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	 return (0);
 
 }
