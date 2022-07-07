97de3b7e012d8fdc5137c5fc36f0778c954d66a7
DinoTech01 <destinyeromosele2018@gmail.com>
2022-07-07 05:47:45 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Test function thats print negative or positive integer
+ * Return: 0
+ */
+
+int main(oid)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
+
+
