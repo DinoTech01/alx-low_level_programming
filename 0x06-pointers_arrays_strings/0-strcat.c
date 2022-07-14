1e243db416db98c6859827ffeb95491cc30562fe
DinoTech01 <destinyeromosele2018@gmail.com>
2022-07-14 16:39:15 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * Return: returns dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+
+	for(i = 0, i != '\0', i++)
+	for(n = 0, n != '\0', n++)
+	{
+		[i + n] = n;
+		n++;
+	}
+	return (dest);
+}
